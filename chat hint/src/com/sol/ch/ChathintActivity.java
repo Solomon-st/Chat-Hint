f1c25a7a5cc9b82bd34100f2896c0d64b825702d
Solomon-st <solomon.li@ymail.com>
2011-11-06 14:07:50 +0800

    first

---
+++ chat hint/src/com/sol/ch/ChathintActivity.java
@@ -0,0 +1,13 @@
+package com.sol.ch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChathintActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
